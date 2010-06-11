95dc30a6ad9f7b0f7cae93462fcb8339aaaf9752
ollinaie <ollinaie@gmx.net>
2010-06-11 23:26:39 +0200

    static.js syntax error corr.

--- static.js
+++ static.js
@@ -8,18 +8,16 @@
  * @codeend
  */
 DocumentJS.Pair.extend('DocumentJS.Static',
-{starts_scope: true,
-},
+{starts_scope: true},
 {
-    
-	add_parent : function(scope){
-        
-        var scope_class=  scope.Class.shortName.toLowerCase();
+    add_parent : function(scope) {
+
+        var scope_class = scope.Class.shortName.toLowerCase();
         this.parent = scope_class == 'class' || scope_class == 'constructor' ? scope : scope.parent;
-        if(scope_class != "file" && this.parent){
+        if (scope_class != "file" && this.parent) {
             this.parent.add(this);
         }
-            
+
     },
     name: 'static'
 });
